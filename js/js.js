ae3cbd92074c117b585656579a2b84ff301b711b
ShakirovMars <mars.mars1996@mail.ru>
2017-04-25 23:32:52 +0300

    fix

--- js/js.js
+++ js/js.js
@@ -5,7 +5,13 @@
 
         });
 	
+$(document).ready(function(){
+  $('table tr').on('click', function(e) {
+    $('table tr').removeClass('marked');
+    $(this).addClass('marked');
 
+  });
+});
   
 
     
