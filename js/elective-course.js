3f7a82294b362c041bf853d1626b795b97ffb2cf
Yulia <petrushenko.yu@gmail.com>
2017-04-26 12:28:45 +0300

    update ALLLLL

---
+++ js/elective-course.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.footable').footable();
+
+
+});
+
+$(document).ready(function(){
+    $('table tr').on('click', function(e) {
+        $('table tr').removeClass('marked');
+        $(this).addClass('marked');
+
+    });
+});
